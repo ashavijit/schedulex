0226eeaa0e27d823218ebc51d8ba70630d363f22
ashavijit <as2440@it.jgec.ac.in>
2023-01-24 23:35:28 +0530

    addede prisma(day1)

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth/next";
+
+declare module "next-auth" {
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    username: string;
+    avatar: string;
+  }
+  interface Session {
+    user: User;
+  }
+}
